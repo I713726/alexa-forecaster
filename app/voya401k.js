6e7c7877594f9f6d5d5b5ccc3825e68b14027e48
Kunkalaguntla <617536@cognizant.com>
2018-03-01 18:28:41 -0500

    added intend

--- app/voya401k.js
+++ app/voya401k.js
@@ -12,7 +12,7 @@ module.exports = function(req, res) {
         console.log('In side LaunchRequest :\n', req.body.request.type);
 		res.json(
             buildResponse(
-                { dateRequested: true },
+                {},
                 '<speak>Hi Tom, how can I help you with your Voya 401(K) Savings Plan today</speak>',
                 {},
                 false
@@ -29,10 +29,34 @@ module.exports = function(req, res) {
         * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
     } else if (req.body.request.type === 'IntentRequest' &&
-               req.body.request.intent.name === 'VoyaHowMyAccountIntent') {
-				res.json( 
-					buildResponse( {}, '<speak>Sure Tom, As of February 15, 2018, your account balance is $55,000. Your rate of return for the past 12 months is 20%, which is above the average portfolio benchmark for this period. Nice job making your money work for you! It looks like you are currently projected to have enough money to retire at age 70. Would you like to hear suggestions to be able retire a little sooner?</speak>', {}, true )
-					);
+	   req.body.request.intent.name === 'VoyaHowMyAccountIntent') {
+		res.json( 
+			buildResponse( { questionNo: '1' }, '<speak>Sure Tom, As of February 15, 2018, your account balance is $55,000. Your rate of return for the past 12 months is 20%, which is above the average portfolio benchmark for this period. Nice job making your money work for you! It looks like you are currently projected to have enough money to retire at age 70. Would you like to hear suggestions to be able retire a little sooner?</speak>', {}, false )
+			);
+    } else if (req.body.request.type === 'IntentRequest' &&
+	   req.body.request.intent.name === 'VoyaYesIntent') {
+		if (req.body.session.attributes && req.body.session.attributes.questionNo && req.body.session.attributes.questionNo == '1') {
+			res.json( 
+				buildResponse( { questionNo: '2' }, '<speak>You are doing a great job of saving 6% from your pay but if you increase your savings rate to 8% you could retire at age 67.  Would you like me to increase your savings rate by 2% now?</speak>', {}, false )
+				);
+		} else if (req.body.session.attributes && req.body.session.attributes.questionNo 
+			&& (req.body.session.attributes.questionNo == '2' || req.body.session.attributes.questionNo == '3')) {
+			res.json( 
+				buildResponse( {}, '<speak>Ok, great. I’ve done that for you. Congratulations your future self will thank you!</speak>', {}, true )
+				);
+		}
+    } else if (req.body.request.type === 'IntentRequest' &&
+	   req.body.request.intent.name === 'VoyaNoIntent') {
+		if (req.body.session.attributes && req.body.session.attributes.questionNo 
+			&& (req.body.session.attributes.questionNo == '1' || req.body.session.attributes.questionNo == '3')) {
+			res.json( 
+				buildResponse( {}, '<speak>Ok, I understand thank you for using Voya 401k service, have a nice day!</speak>', {}, true )
+				);
+		} else if (req.body.session.attributes && req.body.session.attributes.questionNo && req.body.session.attributes.questionNo == '2') {
+			res.json( 
+				buildResponse( {questionNo: '3'}, '<speak>Ok, I understand.  Would you want to Save More in the future? I can sign you up to save 1% more a year from now?</speak>', {}, false )
+				);
+		}
     } else {
         console.error('Intent not implemented: ', req.body);
         res.status(504).json({ message: 'Intent Not Implemented' });
