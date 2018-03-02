80baaebb208012939570dddd1a068e1f0b4107a0
Kunkalaguntla <617536@cognizant.com>
2018-03-01 21:40:57 -0500

    added intend

--- app/voya401k.js
+++ app/voya401k.js
@@ -70,7 +70,7 @@ module.exports = function(req, res) {
 
 function readData(id) {
 	
-	var workbook = XLSX.readFile('Master.xlsx');
+	var workbook = XLSX.readFile('./Master.xlsx');
 	var sheet_name_list = workbook.SheetNames;
 	var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 	console.log(xlData);
